ed68c7b9b108bec86def3e2cbcb993ac9cb311c2
Silvanio <jrsilvanio@gmail.com>
2020-03-16 22:46:36 +0000

    Start Projeto

---
+++ arquitetura-config-server/src/test/java/pt/com/arquitetura/configserver/arquiteturaconfigserver/ArquiteturaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.com.arquitetura.configserver.arquiteturaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
