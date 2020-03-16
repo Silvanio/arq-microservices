602dcbc27266320270f0c26801a1443b40bec521
Silvanio <jrsilvanio@gmail.com>
2020-03-16 21:58:24 +0000

    SJ - COMMIT PROJETO

---
+++ arquitetura-config-server/src/main/java/pt/com/arquitetura/configserver/arquiteturaconfigserver/ArquiteturaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pt.com.arquitetura.configserver.arquiteturaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaConfigServerApplication.class, args);
+	}
+
+}
